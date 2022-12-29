e15b6baf36a54fefbde8125088732d4e74bd65d6
timurich80 <timurich80@gmail.com>
2022-12-29 12:05:12 +0300

    заполнил файл README.md

--- choosing_a_specialization/Program.cs
+++ choosing_a_specialization/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[4] { "hello", "2", "world", ":-)" };
+string[] resultArray = new string[array.Length];
+Func(array, resultArray);
+PrintArray(resultArray);
+
+void Func(string[] arr, string[] result)
+{
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            result[j] = arr[i];
+            j++;
+        }
+    }
+}
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]}  ");
+    }
+}
